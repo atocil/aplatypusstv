fbec0923ee546b5da585a5e86cfa03e797f1ab82
jmklonowski <klonowskijoseph@gmail.com>
2015-10-21 21:14:44 -0700

    Moved donations to jade file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SpeakEZ' });
 });
 
+router.get('/donation', function(req, res, next) {
+    res.render('donation', {title: 'SpeadEZ'});
+});
+
 module.exports = router;
