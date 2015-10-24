43f9c46fa91870794d6bf52aaffadaaba67fe91a
jmklonowski <klonowskijoseph@gmail.com>
2015-10-23 23:29:53 -0700

    Moved secrets server side

--- routes/donations.js
+++ routes/donations.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SpeakEZ' });
+    res.render('donation');
 });
 
 module.exports = router;
