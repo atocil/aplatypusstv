6bbd3d9c61ef15edd3dbeb0026a3088cff2b1012
jmklonowski <klonowskijoseph@gmail.com>
2015-10-26 21:31:55 -0700

    Rest of donations

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var PropertiesReader = require('properties-reader');
+var session = require('express-session');
+var socket_io = require('socket.io');
+var socket_io_client = require('socket.io-client');
+var properties = PropertiesReader('aplatypuss.properties');
+var donations = require('./routes/donations');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -14,16 +18,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Set app.io so it's available for use in bin/www
+var io = socket_io();
+app.io = io;
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: properties.get('sessionSecret')}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/donations', donations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +67,33 @@ app.use(function(err, req, res, next) {
   });
 });
 
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+//DONATIONS SOCKETS
+
+var streamtip_socket = socket_io_client.connect('https://streamtip.com', {
+  query: 'client_id=' +
+  encodeURIComponent(properties.get('streamtip_client_id')) +
+  '&access_token=' +
+  encodeURIComponent(properties.get('streamtip_access_token'))
+});
+
+var donation_socket = io.of('/donations');
+
+streamtip_socket.on('newTip', function(data) {
+  console.log('WE GOT A TIP: ' + data.amount);
+  donation_socket.emit('newTipEvent', data);
+});
+
+
+donation_socket.on('connection', function(socket){
+
+  socket.join('donations');
+  socket.emit('news', { hello: 'world'});
+  socket.on('disconnect', function() {
+    socket.leave('donations');
+  });
+
+});
 
 module.exports = app;
